bfc6cd113e49aba02e5fa57c6fd8f6414620cbd5
Raj Shinde <rajshinde2820@gmail.com>
2023-09-23 12:01:03 +0530

    multiple return value

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,3 +1,30 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardsSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:] // multiple return values
+}
--- Cards/main.go
+++ Cards/main.go
@@ -1,21 +1,26 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spaces"
 	// card := newCard()
 	// card = "Five of Diamonds"
-	cards := []string{newCard(),newCard()} 
-	cards = append(cards, "six of spades")
-	for i, card := range cards{
-		fmt.Println(i,card)
-	}
-	
+	// cards := deck{newCard(), newCard()}
+	// cards = append(cards, "six of spades")
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	cards := newDeck()
+	// cards.print()
 	// fmt.Println(cards)
+
+		// ******************** multiple return values
+	hand, remainingCards := deal(cards, 5)
+  
+	hand.print()
+	remainingCards.print()
+
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
-
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
