5a0114e92c999c5f8fd897c3fa43abe17571eecd
Raj Shinde <rajshinde2820@gmail.com>
2023-10-15 14:20:43 +0530

    push

--- Cards/deck.go
+++ Cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -57,11 +58,15 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffel() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		// swapping
 		d[i], d[newPosition] = d[newPosition], d[i]
-		
+
 	}
 }
